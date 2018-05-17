f95168423bd4c68dfb364227a6e3d1d939cf9936
Harry Cheng <26018+zheharry@users.noreply.github.com>
2018-05-17 22:21:32 +0800

    Update stockquote.py

--- storage/downloads/python/stockquote.py
+++ storage/downloads/python/stockquote.py
@@ -4,22 +4,25 @@ import re
 def get_quote(symbol):
     base_url = 'https://www.bloomberg.com/quote/' + symbol
     content = urllib.urlopen(base_url).read()
+    #print content
     p = re.search('class="priceText_(.*?)">(.*?)<', content)
     a = re.search('class="changeAbsolute_(.*?)">(.*?)<', content)
     b = re.search('class="changePercent_(.*?)">(.*?)<', content)
+    c = re.search('class="price-container (.*?)"', content)
     e = re.search('class="price">(.*?)<', content)
     d = re.search('class="change-container">.*?<div>(.*?)</div>.*?<div>(.*?)</div>', content)
-    #print p, a, b, e, d
+    #print p, a, b, c, e, d
     if p and a and b: # stocks
         price = p.group(2)
         abs   = a.group(2)
         per = b.group(2)
         print symbol, price, abs
         return symbol+' '+price+' '+abs
-    elif e and d: # ETFs
+    elif c and e and d: # ETFs
         price = e.group(1)
         abs = d.group(1)
         per = d.group(2)
+        sign = "+" if c.group(1) == "up" else "-"
         #print symbol, price, abs, per
-        print symbol, price
-        return symbol+' '+price
+        print symbol, price, sign+abs.lstrip()
+        return symbol+' '+price+' '+sign+abs.lstrip()
